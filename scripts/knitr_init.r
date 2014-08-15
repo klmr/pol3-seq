9254af968c3557d23d150a94781d81209b22d05a
Konrad Rudolph <konrad_rudolph@madrat.net>
2014-08-15 16:38:33 +0100

    Start structuring knitr code

---
+++ scripts/knitr_init.r
@@ -0,0 +1,7 @@
+library(knitr)
+
+# Adjust knitr paths to my project folder structure
+root_dir = normalizePath(file.path(getwd(), '..'))
+base_dir = file.path(root_dir, 'results', 'report')
+opts_knit$set(root.dir = root_dir,
+              base.dir = base_dir)
